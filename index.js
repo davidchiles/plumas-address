31352945fedc2a6f5db39f261c280b9b929fcf1f
David Chiles <david@davidchiles.org>
2016-11-28 17:57:14 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var AdmZip = require('adm-zip');
+var xml2js = require('xml2js');
+var json2csv = require('json2csv');
+var fs = require('fs');
+
+
+var nopt = require("nopt")
+  , path = require("path")
+  , knownOpts = { "zip" : path, "out" : path}
+  , parsed = nopt(knownOpts)
+
+var zip = new AdmZip(parsed.zip);
+var zipEntries = zip.getEntries();
+
+var parser = new xml2js.Parser();
+
+zipEntries.forEach(function(zipEntry) {
+	//Unzip and get kml file. Should be only file.
+	if (zipEntry.entryName == "doc.kml") {
+		var dataString = zipEntry.getData().toString('utf8'); 
+		//Parse XML
+		parser.parseString(dataString, function (err, result) {
+			//Traverse file hierarchy to get to placemakrs where are the real address data lives
+        	var addresses = result.kml.Document[0].Folder[1].Placemark.map(function(placemark){
+				var result = {}
+				var pointArray = placemark.Point[0].coordinates[0].split(',');
+				result.lat = pointArray[0];
+				result.lon = pointArray[1];
+				placemark.ExtendedData[0].SchemaData[0].SimpleData.forEach(function(data) {
+					result[data.$.name] = data._
+				});
+					return result
+			});
+			var fields = Object.keys(addresses[0])
+			var csvString = json2csv({ data: addresses, fields: fields});
+  			fs.writeFileSync(parsed.out,csvString)
+    	});
+	}
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+#Plumas-Address
+
+This converts [Plumas County address point KMZ](http://plumascounty.us/index.aspx?NID=2199) file to a more usable csv.
+
+[Results](Data/results.csv)
+
+### Install
+
+`npm install`
+
+### Usage
+
+`node index.js -zip ./Data/Parcel\ Address\ Points\ \(2016\)_201607131743099365.kmz -out ./Data/results.csv`
